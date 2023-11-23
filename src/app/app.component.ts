aa3e3d09b37b67d3bea0d25284c6d43766c7616d
Júlia Assis <julia.bhte@gmail.com>
2023-11-23 19:50:59 -0300

    projeto simulador pipeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pipeline-simulador';
+export class AppComponent implements OnInit {
+  numeroInstrucoes:number = 0;
+  listaInstrucoes:{index:string,color:string}[][] = [];
+  baseInstrucoes:{index:string,color:string}[] = [];
+  tempoTotal:string = '-';
+  tempoCiclo:number = 0;
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  iniciarSimulacao(){
+    this.listaInstrucoes = [];
+    this.runTime();
+    this.tempoTotal = ((this.numeroInstrucoes+4)*this.tempoCiclo).toString()+'ns';
+  }
+
+  getListInstrucoes(){
+    let listBaseTodasIntrucoes:{index:string,color:string}[][] = []
+
+    for(let i = 0; i<5; i++){
+      listBaseTodasIntrucoes[i] = [{index:'  ',color:'background-color: white;'},{index:'  ',color:'background-color: white;'},{index:'  ',color:'background-color: white;'},{index:'  ',color:'background-color: white;'}]
+      this.listaInstrucoes[i] = [];
+    }
+    for(let i=Number(this.numeroInstrucoes);i>=1;i--){
+      const color = this.getRandomColor();
+      listBaseTodasIntrucoes[0].splice(4,0,{index:i.toString(),color:'background-color:'+color});
+      listBaseTodasIntrucoes[1].splice(3,0,{index:i.toString(),color:'background-color:'+color});
+      listBaseTodasIntrucoes[2].splice(2,0,{index:i.toString(),color:'background-color:'+color});
+      listBaseTodasIntrucoes[3].splice(1,0,{index:i.toString(),color:'background-color:'+color});
+      listBaseTodasIntrucoes[4].splice(0,0,{index:i.toString(),color:'background-color:'+color});
+
+    }
+    
+    return listBaseTodasIntrucoes.slice(0).reverse();
+    
+  }
+  runTime(){
+    let listBaseTodasIntrucoes:{index:string,color:string}[][] = this.getListInstrucoes();
+    for(let i=0; i<Number(this.numeroInstrucoes)+4;i++){
+
+      setTimeout(()=> {
+
+        this.listaInstrucoes[0].push(listBaseTodasIntrucoes[0][i])
+        this.listaInstrucoes[1].push(listBaseTodasIntrucoes[1][i])
+        this.listaInstrucoes[2].push(listBaseTodasIntrucoes[2][i])
+        this.listaInstrucoes[3].push(listBaseTodasIntrucoes[3][i])
+        this.listaInstrucoes[4].push(listBaseTodasIntrucoes[4][i])
+
+      }, i*1000);  
+      
+    }
+  }
+
+  getRandomColor() {
+    var color = Math.floor(Math.random() * 16777216).toString(16);
+    return '#000000'.slice(0, -color.length) + color;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    FormsModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
